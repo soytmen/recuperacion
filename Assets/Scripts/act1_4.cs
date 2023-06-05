ddfc1275ab9b3f25d6bc3f6d9c1ad2832b481c3f
soytmen <114388813+soytmen@users.noreply.github.com>
2023-06-05 08:32:19 +0200

    1

---
+++ Assets/Scripts/act1_4.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class act1_4: MonoBehaviour
+{
+    public int points;
+    private float speed = 1.5f;
+    [SerializeField]
+    private bool isGameOver;
+     public string entrada = "Bienvenido";
+}
